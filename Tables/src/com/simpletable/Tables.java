21ea031af8d11755c00e197f43d998b123936a32
JUSTIN <JUSTIN@DESKTOP-LO4A1PN>
2021-08-03 14:40:44 -0300

    commit

---
+++ Tables/src/com/simpletable/Tables.java
@@ -0,0 +1,14 @@
+package com.simpletable;
+
+public class Tables {
+
+	public static void main(String[] args) {
+		
+		
+		for(int i = 1; i<=10; ++i) {System.out.println("");
+			for(int x = 1; x<=10;++x) {
+			System.out.println(x + " * " + i + " = " + (x * i));
+					}
+		}
+	}
+}
---
+++ Tables/src/com/simpletable/Tables2.java
@@ -0,0 +1,10 @@
+package com.simpletable;
+
+public class Tables2 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
